e8754995f05486f85856fb0903e3e701b3eac0b3
Ajay Gandecha <ajgandecha@gmail.com>
2023-02-20 10:12:42 -0500

    Removed usage, kept rules argument err

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -84,18 +84,7 @@ else {
         );
         console.log(
             `
-            Usage: node-rps [SHOT]
-            Play Rock Paper Scissors (RPS)
             
-              -h, --help      display this help message and exit
-              -r, --rules     display the rules and exit
-            
-            Examples:
-              node-rps        Return JSON with single player RPS result.
-                              e.g. {"player":"rock"}
-              node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                              e.g {"player":"rock","opponent":"scissors","result":"win"}
-    
             Rules for Rock Paper Scissors:
     
             - Scissors CUTS Paper

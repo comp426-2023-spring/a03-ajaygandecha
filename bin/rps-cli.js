195eae49ea49a4d981d1ab0c4e8360fa5f512494
Ajay Gandecha <ajgandecha@gmail.com>
2023-02-20 10:10:11 -0500

    Added help section to invalid argument section bc autograder

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -82,5 +82,26 @@ else {
             Acceptable arguments: rock | paper | scissors
             `
         );
+        console.log(
+            `
+            Usage: node-rps [SHOT]
+            Play Rock Paper Scissors (RPS)
+            
+              -h, --help      display this help message and exit
+              -r, --rules     display the rules and exit
+            
+            Examples:
+              node-rps        Return JSON with single player RPS result.
+                              e.g. {"player":"rock"}
+              node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                              e.g {"player":"rock","opponent":"scissors","result":"win"}
+    
+            Rules for Rock Paper Scissors:
+    
+            - Scissors CUTS Paper
+            - Paper COVERS Rock
+            - Rock CRUSHES Scissors          
+            `
+        );
     }
 }
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -95,5 +95,34 @@ else {
             Acceptable arguments: rock | paper | scissors | lizard | spock
             `
         );
+        console.log(
+            `
+            Usage: node-rpsls [SHOT]
+            Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+            
+              -h, --help        display this help message and exit
+              -r, --rules       display the rules and exit
+            
+            Examples:
+              node-rpsls        Return JSON with single player RPSLS result.
+                                e.g. {"player":"rock"}
+              node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                                e.g {"player":"rock","opponent":"Spock","result":"lose"}
+    
+            Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+    
+            - Scissors CUTS Paper
+            - Paper COVERS Rock
+            - Rock SMOOSHES Lizard
+            - Lizard POISONS Spock
+            - Spock SMASHES Scissors
+            - Scissors DECAPITATES Lizard
+            - Lizard EATS Paper
+            - Paper DISPROVES Spock
+            - Spock VAPORIZES Rock
+            - Rock CRUSHES Scissors                    
+            `
+        );
+    
     }
 }
